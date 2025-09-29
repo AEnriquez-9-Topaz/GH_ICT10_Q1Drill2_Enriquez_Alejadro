6e60175da61c4250a708b295532c0e9a29f4c03f
AJ (Alejandro)_10-Topaz <enriquez.alm@obmontessori.edu.ph>
2025-09-29 09:40:36 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+# data types for Drill
+from pyscript import display, document
+
+
+#Variable
+footer = {'Enriquez | 10-Topaz'} #dict
+
+# Business Hours
+display(footer, target="bottom")
+
+# Order Form
+def studentform(e): #creating calculating functions in python
+    document.getElementById("profile").innerHTML = "" #clearing the output field
+
+    Address = document.getElementById("address").value.strip()
+    Age = document.getElementById("age").value.strip()
+    Name = document.getElementById("name").value.strip()
+    Email = document.getElementById("email").value.strip()
+
+
+    # Validating if they completed the input field
+    if not Name or not Email or not Age or not Address:
+        display("Please fill out personal details before checkout", target="profile")
+        return
+
+    # Displays
+    display(f"Name: {Name}", target="Name")
+    display(f"Email: {Email}", target="Email")
+    display(f"Age: {Age}", target="Age")
+    display(f"Address: {Address}", target="Address") #Extra input field
+    
+
+    # Multiline display
+    display(
+    f"""Hello, {Name}!
+    Your student\'s address is {Address}
+    your student age is {Age}.
+    Your email, {Email}, will be confirmed shortly. Remember, \" Education is the most powerful weapon which you can use to change the world.\" - Nelson Mandela""",
+    target="profile"
+)
+
+    
+
+
+
+  
